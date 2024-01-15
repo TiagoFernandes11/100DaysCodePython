cf31465f50d78aaf5e5358b2049088a67fd6a649
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-01-15 17:34:00 -0300

    Hello World + BandNameGenarator (Day 1)

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+cityInput = input("What's the name of the city you grew up in?")
+petNameInput = input("What's your pet's name?")
+print("Your band name could be", cityInput, petNameInput)
