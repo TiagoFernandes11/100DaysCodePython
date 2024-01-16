2d0cec112a5ee1a9a2021263786d30625a69e91c
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-01-16 16:47:10 -0300

    Update Hangman.py

--- Day7/Hangman.py
+++ Day7/Hangman.py
@@ -1,20 +1,41 @@
 import Constants, random
 
+lives = 6
+tryedLetters = []
 
 def chooseRandomWord():
     return Constants.words[random.randint(0, len(Constants.words)-1)]
 
-def generateBoard(word):
+def renderBoard(word, letter_list):
+    print(Constants.stages[lives])
     s = ''
     for i in range(len(word)):
-        s += '_ '
-    return s
+        if word[i] in letter_list:
+            s += word[i] + " "
+        else:
+            s += '_ '
+    print(s + "\n")
+
+def addUserLetterInput():
+    global lives, word
+    inputLetter = input("Guess a letter: ")
+    if len(inputLetter) > 1:
+        print("Only one letter")
+    else:
+        tryedLetters.append(inputLetter)
+        if word.find(inputLetter) < 0:
+            lives -= 1
 
 word = chooseRandomWord()
 
-board = generateBoard(word)
-
 print(Constants.logo)
-print(word)
-print(generateBoard(word))
+
+while lives > 0:
+    renderBoard(word, tryedLetters)
+    addUserLetterInput()
+    if lives == 0:
+        print(Constants.stages[0])
+        print("Game over")
+
+
 
