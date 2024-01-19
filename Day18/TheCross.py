785c1b479ce055c3169fd9d8e499d7511317c81e
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-01-19 11:49:08 -0300

    Create TheCross.py

---
+++ Day18/TheCross.py
@@ -0,0 +1,31 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(30)
+
+
+
