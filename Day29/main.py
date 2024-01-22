02e7d65edb47ccff7c74c3916f666eb863bb94f6
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-01-22 12:35:47 -0300

    GUI Layout complete

--- Day29/main.py
+++ Day29/main.py
@@ -7,24 +7,24 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=100, pady=50)
+window.config(padx=20, pady=20)
 
-canvas = Canvas(width=260, height=200, highlightthickness=0)
+canvas = Canvas(width=200, height=200, highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(130, 80, image=logo_img)
-canvas.grid(column=0, row=0)
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=0, row=0, columnspan=3)
 
 website_input_label = Label(text="Website")
 website_input_label.grid(column=0, row=1)
 
-website_input = Entry()
-website_input.grid(column=1, row=1)
+website_input = Entry(width=40)
+website_input.grid(column=1, row=1, columnspan=2)
 
 username_input_label = Label(text="Email/Username")
 username_input_label.grid(column=0, row=2)
 
-username_input = Entry()
-username_input.grid(column=1, row=2)
+username_input = Entry(width=40)
+username_input.grid(column=1, row=2, columnspan=2)
 
 password_input_label = Label(text="Password")
 password_input_label.grid(column=0, row=3)
@@ -35,7 +35,7 @@ password_input.grid(column=1, row=3)
 generate_password_button = Button(text="generate password")
 generate_password_button.grid(column=2, row=3)
 
-add_button = Button(text="add")
+add_button = Button(text="add", width=10)
 add_button.grid(column=1, row=4)
 
 
