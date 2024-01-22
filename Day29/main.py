3cf5a2dce5b9801f4a78a8633b8d0e0a6e0eeff4
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-01-22 12:39:27 -0300

    generate_password function

--- Day29/main.py
+++ Day29/main.py
@@ -1,6 +1,35 @@
 from tkinter import *
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    final_password = ""
+
+    print("Welcome to the PyPassword Generator!")
+    nr_letters = int(input("How many letters would you like in your password?\n"))
+    nr_numbers = int(input(f"How many numbers would you like?\n"))
+    nr_symbols = int(input(f"How many symbols would you like?\n"))
+
+    randomizer = []
+
+    for i in range(nr_letters + nr_numbers + nr_symbols):
+        randomizer.append(random.randint(0, 2))
+
+    for i in range(len(randomizer)):
+        if randomizer[i] == 0:
+            final_password += letters[random.randint(0, len(letters) - 1)]
+        elif randomizer[i] == 1:
+            final_password += numbers[random.randint(0, len(numbers) - 1)]
+        elif randomizer[i] == 2:
+            final_password += symbols[random.randint(0, len(symbols) - 1)]
+
+    return final_password
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
