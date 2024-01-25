62d34ef859bb38d8709518283e9d8173ec421c16
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-01-25 09:23:15 -0300

    Top 100 Billboard by date added by spotfy playlist

--- Day46/main.py
+++ Day46/main.py
@@ -1,41 +1,48 @@
+from bs4 import BeautifulSoup
 import requests
-from bs4 import  BeautifulSoup
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
-# top 100 billboard a partir de yyyy-MM-dd
-
-URL = "https://www.billboard.com/charts/hot-100/"
-
+# Scraping Billboard 100
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-response = requests.get(URL + date)
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
 
-website_html = response.text
+#Spotify Authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id="YOUR CLIENT ID",
+        client_secret="YOUR CLIENT SECRET",
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
 
-soup = BeautifulSoup(website_html, "html.parser")
+#Searching Spotify for songs by title
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+    except TypeError:
+        print("TypeError: something went wrong")
+    except UnicodeEncodeError:
+        print("UnicodeEncodeError: song skiped")
 
-all_songs = soup.select("li ul li h3")
+#Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
 
-#interação com spotfy
-
-SPOTFY_CLIENT_ID = "SPOTFY_CLIENT_ID"
-SPOTFY_SECRET_KEY = "SPOTFY_SECRET_KEY"
-
-song_names = [song.getText().strip() for song in all_songs]
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTFY_CLIENT_ID,
-                                                           client_secret=SPOTFY_SECRET_KEY))
-
-lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
-results = sp.artist_top_tracks(lz_uri)
-
-for track in results['tracks'][:10]:
-    print('track    : ' + track['name'])
-    print('audio    : ' + track['preview_url'])
-    print('cover art: ' + track['album']['images'][0]['url'])
-    print()
-
-
-
-
-
+#Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
