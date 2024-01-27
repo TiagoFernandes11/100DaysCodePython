2fecec35052725aff5cd1eccf43789ae6e9dc31d
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-01-27 19:15:13 -0300

    Create hello.py

---
+++ Day55/hello.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def make_bold(function):
+    def bold():
+        return "<h1><b>" + function() + "</b></h1>"
+
+    return bold
+
+
+@app.route("/")
+def hello_world():
+    return '<h1 style= "text-align: center">Hello, World!</h1>'
+
+@app.route("/bye")
+@make_bold
+def bye():
+    return "bye!"
+
+
+@app.route("/username/<name>/<int:number>")
+def hello_user(name, number):
+    return f"<p>Hello, {name}! {number}</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
