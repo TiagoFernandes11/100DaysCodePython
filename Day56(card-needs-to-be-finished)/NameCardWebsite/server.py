642e961719398526647cea3b5a75e8da20bd8f58
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-01-29 10:35:08 -0300

    Sending emails

--- Day56(card-needs-to-be-finished)/NameCardWebsite/server.py
+++ Day56(card-needs-to-be-finished)/NameCardWebsite/server.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+import smtplib
+import requests
 
 app = Flask(__name__)
 
@@ -7,6 +8,24 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template("index.html")
+
+
+@app.route("/form-entry", methods=["POST"])
+def receive_contact():
+    name = request.form["name"]
+    email = request.form["email"]
+    subject = request.form["subject"]
+    message = request.form["message"]
+    send_email(name, email, subject, message)
+    return "<h1>Successfully sent your message</h1>"
+
+
+def send_email(name, email, subject, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {subject}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == "__main__":
